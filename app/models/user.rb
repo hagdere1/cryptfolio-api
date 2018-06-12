2f4fc7f39140869adfea2eb0348ccd2fdc534aba
Haik Gregory Agdere <hagdere1@binghamton.edu>
2018-06-12 13:53:58 -0400

    Add show action to users controller.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+
   has_one :portfolio, dependent: :destroy
   has_many :holdings, through: :portfolios
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   namespace :api, defaults: { format: :json },
                   constraints: { subdomain: :api }, path: '/' do
-    scope module: :v1 do
-      
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+      resources :users, :only => [:show]
     end
   end
 end
