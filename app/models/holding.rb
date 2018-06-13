f8b2045166ad97ef9494b9cd43263758010844f7
Haik Gregory Agdere <hagdere1@binghamton.edu>
2018-06-13 10:20:21 -0400

    Uniqueness validation for coin holdings.

--- app/models/holding.rb
+++ app/models/holding.rb
@@ -1,5 +1,7 @@
 class Holding < ApplicationRecord
   validates :quantity, :portfolio_id, :coin_id, presence: true
+  validates :coin_id, uniqueness: { scope: :portfolio_id,
+                                    message: 'no duplicate coin per porfolio' }
 
   belongs_to :portfolio
   belongs_to :coin
