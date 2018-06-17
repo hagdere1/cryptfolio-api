628d9d80fba318f7db293af6eda708c262477607
Haik Gregory Agdere <hagdere1@binghamton.edu>
2018-06-17 11:25:30 -0400

    Add token expiration.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::API
   private
     def authenticate_token
       authenticate_with_http_token do |token, options|
-        User.find_by(auth_token: token)
+        User.find_by(auth_token: token).where("token_created_at >= ?", 1.month.ago).first
       end
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_17_151015) do
+ActiveRecord::Schema.define(version: 2018_06_17_151930) do
 
   create_table "coins", force: :cascade do |t|
     t.string "name"
@@ -59,7 +59,8 @@ ActiveRecord::Schema.define(version: 2018_06_17_151015) do
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
     t.string "auth_token"
-    t.index ["auth_token"], name: "index_users_on_auth_token"
+    t.datetime "token_created_at"
+    t.index ["auth_token", "token_created_at"], name: "index_users_on_auth_token_and_token_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
