d8e94128ed0b590390a45a77bfb9c42209bf9b83
Haik Gregory Agdere <hagdere1@binghamton.edu>
2018-07-02 22:39:01 -0400

    Add trades#index action.

--- app/controllers/api/v1/trades_controller.rb
+++ app/controllers/api/v1/trades_controller.rb
@@ -2,6 +2,21 @@ module Api
   module V1
     class TradesController < ApplicationController
       def index
+        trades = Trade.where(user_id: current_user.id)
+        data = Array.new
+
+        trades.each do |trade|
+          data.push({
+            sell_quantity: trade.sell_quantity,
+            sell_coin: Coin.find_by(id: trade.sell_coin_id),
+            buy_quantity: trade.buy_quantity
+            buy_coin: Coin.find_by(id: trade.buy_coin_id),
+            exchange: trade.exchange,
+            timestamp: trade.timestamp
+          })
+        end
+
+        render json: { status: "success", data: data }, status: 200
       end
 
       def create
@@ -12,6 +27,11 @@ module Api
 
       def destroy
       end
+
+      private
+        def trade_params
+          params.require(:trade).permit(:sell_quantity, :sell_coin_id, :buy_quantity, :buy_coin_id, :user_id, :exchange, :timestamp)
+        end
     end
   end
 end
