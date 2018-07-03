1d2a46bc128a668d3dabd0ef333b139c6f071c86
Haik Gregory Agdere <hagdere1@binghamton.edu>
2018-07-02 21:36:48 -0400

    Create trade and transfer controllers.

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
       resources :holdings, only: [:create, :update, :destroy]
       resources :sessions, only: [:create, :destroy]
       resources :portfolio, only: [:index]
+      resources :trades, only: [:index, :create, :update, :destroy]
+      resources :transfers, only: [:index, :create, :update, :destroy]
       get '/latest_prices' => 'prices#latest_prices'
       get '/historical_prices' => 'prices#historical_prices'
     end
